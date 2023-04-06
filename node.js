5160be47e83217a601ce72dec415e1b2d98c9dbf
SpeedyEggbert <126017385+SpeedyEggbert@users.noreply.github.com>
2023-04-06 17:35:37 -0400

    Fixed pathing and linked css files in the main

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.listen(3000, () => {
   console.log("Application started and Listening on port 3000");
@@ -12,18 +13,36 @@ app.get("/", (req, res) => {
 app.get("/home", (req, res) => {
   // res.redirect(__dirname);
 });
+app.get("/home.css", (req, res) => {
+  res.sendFile(path.join(__dirname + "/pages/home/home.css"));
+});
 
 app.get("/apply", (req, res) => {
   res.sendFile(__dirname + "/pages/apply/apply.html");
 });
+app.get("/apply.css", (req, res) => {
+  res.sendFile(path.join(__dirname + "/pages/apply/apply.css"));
+});
+
 app.get("/floor", (req, res) => {
   res.sendFile(__dirname + "/pages/floor/floor.html");
 });
+app.get("/floor.css", (req, res) => {
+  res.sendFile(path.join(__dirname + "/pages/floor/floor.css"));
+});
+
 app.get("/tour", (req, res) => {
   res.sendFile(__dirname + "/pages/tour/tour.html");
 });
+app.get("/tour.css", (req, res) => {
+  res.sendFile(path.join(__dirname + "/pages/tour/tour.css"));
+});
+
 app.get("/resident-links", (req, res) => {
   res.sendFile(__dirname + "/pages/resident-links/resident-links.html");
+});
+app.get("/resident-links.css", (req, res) => {
+  res.sendFile(path.join(__dirname + "/pages/resident-links/resident-links.css"));
 });
 
 app.get("/resident-links/forum", (req, res) => {
