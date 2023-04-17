04d56641cde85f4f18fcd6cc9dda24f0cd73f0e6
Hunter Jones <hunterj0915@gmail.com>
2023-04-17 14:31:58 -0400

    Add js message when form completion for apply and tour pages

---
+++ public/js/apply.js
@@ -0,0 +1,13 @@
+	let form = document.getElementById('apply_form')
+
+	console.log(form)
+	form.addEventListener('submit', (e) => {
+
+		e.preventDefault()
+
+		let name = document.getElementById('name').value
+
+		document.getElementById('result').innerHTML = `
+			<p>Thank you ${name} for submitting your payment</p>
+			`
+    })
---
+++ public/js/tour.js
@@ -0,0 +1,17 @@
+
+	let form = document.getElementById('tour_form')
+console.log(form)
+	form.addEventListener('submit', (e) => {
+
+		e.preventDefault()
+
+		let first_name = document.getElementById('first_name').value
+		let last_name = document.getElementById('last_name').value
+		let time = document.getElementById('time').value
+		let date = document.getElementById('date').value
+        let phone = document.getElementById('phone_num').value
+
+		document.getElementById('result').innerHTML = `
+			<p>Thank you ${first_name} ${last_name} for submitting your request. We will get back to you soon at ${phone}.</p>
+			`
+    })
